4c8096eccd55c9835df6506de69f9639ed4f0959
Marlayna Verenna <m.verenna@setonhill.edu>
2020-02-03 12:16:48 -0500

    Initial Commit

---
+++ Homework 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 1
+//
+//  Created by Marlayna Verenna on 1/29/20.
+//  Copyright © 2020 Marlayna Verenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
